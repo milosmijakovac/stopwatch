6c8787d94349a4b7e134db7af67747566b109198
Milos Mijakovac <milosmijakovac@gmail.com>
2024-07-20 18:34:47 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './style.scss';
-import Stopwatch from './Stopwatch';
+import React from "react";
+import "./style.scss";
+import Stopwatch from "./Stopwatch";
 
 const App: React.FC = () => {
-  /**
-   * Implement the core of your task in this file.
-   */
-
   return (
     <div className="stopwatch-page">
       <Stopwatch />
